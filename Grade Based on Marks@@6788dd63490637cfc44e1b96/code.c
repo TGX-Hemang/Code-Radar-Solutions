920abaafae6f350247a07bed242a4292fbdd8268
Hemang <hemang1231.becse24@chitkara.edu.in>
2025-02-04 18:13:04 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+char* welcome() {
+    int m;
+    scanf("%d", m);
+    if (m>=90)
+    return "A";
+    else if (m>=80 && m<90)
+    return "B";
+    else if (m>=70 && m<80)
+    return "C";
+    else if (m>=60 && m<70)
+    return "D";
+    else
+    return "F";
+}
+
+int main() {
+    printf("%s", welcome());
+    return 0;
+}
