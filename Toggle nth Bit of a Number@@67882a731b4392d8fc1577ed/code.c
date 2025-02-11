bcf5a63690c0d10aa881410d5e48fb9cbe1a6ebe
Hemang <hemang1231.becse24@chitkara.edu.in>
2025-02-11 18:16:17 +0530

    Solution to Toggle nth Bit of a Number

--- Toggle nth Bit of a Number@@67882a731b4392d8fc1577ed/code.c
+++ Toggle nth Bit of a Number@@67882a731b4392d8fc1577ed/code.c
@@ -27,20 +27,20 @@ int main() {
         count++;
         n /= 2;
     }
-    printf("Binary: ");
-    for (int ik=0; ik<=31; ik++) {
-        printf("%d", arr[ik]);
-    }
+    // printf("Binary: ");
+    // for (int ik=0; ik<=31; ik++) {
+    //     printf("%d", arr[ik]);
+    // }
 
     if (arr[bit]==1)
     arr[bit]-=1;
     else 
     arr[bit]+=1;
 
-    printf("\nBinary: ");
-    for (int ik=0; ik<=31; ik++) {
-        printf("%d", arr[ik]);
-    }
+    // printf("\nBinary: ");
+    // for (int ik=0; ik<=31; ik++) {
+    //     printf("%d", arr[ik]);
+    // }
 
     int temp1;
     for (int i = 0; i < 32 / 2; i++) {
@@ -48,13 +48,13 @@ int main() {
     arr[i] = arr[32 - 1 - i];
     arr[32 - 1 - i] = temp1;
 }
-    printf("\nBinary: ");
-    for (int ik=0; ik<=31; ik++) {
-        printf("%d", arr[ik]);
-    }
+    // printf("\nBinary: ");
+    // for (int ik=0; ik<=31; ik++) {
+    //     printf("%d", arr[ik]);
+    // }
 
-    int temp3 = 31;
-    for (int k=0; k<=31; k++) {
+    int temp3 = 32;
+    for (int k=0; k<=32; k++) {
         if (arr[k]==1)
             {
                 if (temp3!=0)
