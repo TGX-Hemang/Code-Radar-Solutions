bd899545b41af96f982b2ac731967e5b23bd98fd
Hemang <hemang1231.becse24@chitkara.edu.in>
2025-02-11 11:07:09 +0530

    Solution to Toggle nth Bit of a Number

--- Toggle nth Bit of a Number@@67882a731b4392d8fc1577ed/code.c
+++ Toggle nth Bit of a Number@@67882a731b4392d8fc1577ed/code.c
@@ -38,10 +38,10 @@ int main() {
     arr[count - 1 - i] = temp1;
 }
 
-    if (arr[count-bit-1]==1)
-    arr[count-bit-1] = 0;
+    if (arr[32-bit-1]==1)
+    arr[32-bit-1] = 0;
     else
-    arr[count-bit-1] = 1;
+    arr[32-bit-1] = 1;
 
     int temp3 = count-1;
     for (int k=0; k<=count-1; k++) {
