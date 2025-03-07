df7b756ce43156f0936dd446703e250d80b7ee47
Hemang <hemang1231.becse24@chitkara.edu.in>
2025-03-07 22:52:51 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,6 +1,6 @@
 char caesarCipher(char msg[], int shift, char encrypted[]) {
     int i, count=0;
-    for(i=0;i<strlen(msg);i++) {
+    for(i=0;i<100;i++) {
         encrypted[i]=msg[i]+shift;
     }
     return encrypted;
