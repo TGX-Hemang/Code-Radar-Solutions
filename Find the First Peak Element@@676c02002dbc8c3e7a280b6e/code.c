9630aa5854203975f0465f16d40993404b152a0e
Hemang <hemang1231.becse24@chitkara.edu.in>
2025-03-08 19:24:09 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -8,13 +8,21 @@ int main() {
     for(i=0;i<len;i++) {
         scanf("%d", &arr[i]);
     }
-    int max = arr[0];
+    int max = arr[0];   // assuming first element as the temporary peak
     for(i=0;i<len;i++) {
-        if (max<arr[i])
-        {max=arr[i];
-        if (max>arr[i+1] || max==arr[len-1])
-        {printf("%d", max);
-        return 0;}}
+        if (max<arr[i]) // checking if assimption is correct or not
+        {
+            max=arr[i]; // got the current peak value till ith index
+
+            // now checking if it really is the peak and return the final output
+            if (max>arr[i+1] || max==arr[len-1])
+            {
+                // it returns the output and ends the main function with return 0
+                printf("%d", max);
+                return 0;
+            }
+        }
     }
+    // incase if it fails, control will shift to this statement
     printf("-1");
 }
