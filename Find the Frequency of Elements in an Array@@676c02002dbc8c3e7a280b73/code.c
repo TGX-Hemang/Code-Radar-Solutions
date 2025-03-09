ad2c2b39792ccc29b88d69de999585dddabb18f2
Hemang <hemang1231.becse24@chitkara.edu.in>
2025-03-09 14:55:42 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 
+int isInArray(int target, int arr[10][2]) {
+    int i;
+    for(i=0;i<10;i++) {
+        if (target==arr[i][0]) 
+            return i;
+    }
+    return -1;
+}
+
 int main() {
-    int n;
-    scanf("%d", &n);
-    int arr[n];
-    int arr2d[20][20];
-    int count =0;
-    for (int i=0; i<n; i++) {
-        scanf("%d", &arr[i]);
-        for (int j=0; j<n; j++) {
-            if (arr[i]==arr2d[j][0]) {
-                arr2d[j][1]+=1;
-                }
-            else {
-                arr2d[j][0]=arr[i];
-                arr2d[j][1]=0; 
-               
-                }} count++;
-        }
+    int size;
+    printf("Enter the size of array: ");
+        scanf("%d", &size);
+
+    int array[size], index;
+    for(index=0;index<size;index++) 
+        scanf("%d", &array[index]);
     
-    for (int i=0; i<count; i++) {
-        printf("%d %d\n", arr2d[i][0], arr2d[i][1]);
+    int ind=0,r,c, count[10][2];
+    for(r=0;r<10;r++) {
+        for(c=0;c<2;c++) count[r][c]=0;
+    }
+    
+    for(r=0;r<size;r++) {
+            int index= isInArray(array[r], count);
+            if (index==(-1)) 
+            {
+                count[ind][0]+=array[r];
+                count[ind][1]=1;
+                ind++;
+            }
+            else count[index][1] += 1;
     }
 
+    for(c=0;c<size;c++) {
+            if (count[c][1]!=0) printf("%d %d\n", count[c][0], count[c][1]);
+    }
 }
