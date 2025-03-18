73df1f84fb699aee8e00514aaf7e498947725abc
Hemang <hemang1231.becse24@chitkara.edu.in>
2025-03-18 11:09:47 +0530

    Solution to Find the Leaders in an Array

--- Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    void leaders(int *rp, int *p, int n);
+    int n, k;
     scanf("%d", &n);
-    int a[n],i;
-    for(i=0;i<n;i++)    
-        scanf("%d", &a[i]);
-    if (a[0]>a[1]) printf("%d ", a[0]);
-    for(i=0;i<n;i++) {
-        if (a[i]>a[(i-1>0)?i-1: 0]&&a[i]>a[(i+1<n)?i+1: n-1])
-            printf("%d ", a[i]);
-    }
-    printf("%d", a[n-1]);
-    
+    int arr[n], i, j;
+    for(i=0;i<n;i++)
+        scanf("%d", &arr[i]);
+
+    int retarr[]={0};
+    leaders(&retarr[0], &arr[0], n);
+    for(i=0;i<n;i++)
+        printf("%d", retarr[i]);
 }
+
+void leaders(int *rp, int *p, int n) {
+    int i;
+    for(i=0;i<n;i++) {
+        if(*(p+i)>*(p-((p-i<0)?0: 1)) && *(p+i)>*(p+((p+i<n)?i: 0)))
+        *(rp++)=*(p+i);
+    }
+}
+
