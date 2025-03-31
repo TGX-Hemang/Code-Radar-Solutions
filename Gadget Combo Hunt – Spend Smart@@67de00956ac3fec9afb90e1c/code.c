2a6ff8c71544cebc28644a028be241c423f5182e
Hemang <hemang1231.becse24@chitkara.edu.in>
2025-03-31 21:54:21 +0530

    Solution to Gadget Combo Hunt – Spend Smart

--- Gadget Combo Hunt – Spend Smart@@67de00956ac3fec9afb90e1c/code.c
+++ Gadget Combo Hunt – Spend Smart@@67de00956ac3fec9afb90e1c/code.c
@@ -2,7 +2,7 @@ int maxGadgetSpend(int ks[], int nk, int hs[], int nh, int budget) {
     int i, j, max;
     for(i=0;i<nk;i++) 
     {
-        max = 0;
+        max = -1;
         for(j=0;j<nh;j++) 
         {
             if (max<ks[i]+hs[j] && ks[i]+hs[j]<=budget) {
