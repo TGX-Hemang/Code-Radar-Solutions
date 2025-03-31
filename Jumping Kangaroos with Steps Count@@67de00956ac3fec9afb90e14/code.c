7c37c15f054b673bb020b36348a6c03c423c427f
Hemang <hemang1231.becse24@chitkara.edu.in>
2025-03-31 22:19:17 +0530

    Solution to Jumping Kangaroos with Steps Count

--- Jumping Kangaroos with Steps Count@@67de00956ac3fec9afb90e14/code.c
+++ Jumping Kangaroos with Steps Count@@67de00956ac3fec9afb90e14/code.c
@@ -1,4 +1,6 @@
 int kangaroo(int x1, int v1, int x2, int v2) {
-    int n = ((x2-x1<0)? x1-x2: x2-x1)/((v2-v1<0)? v1-v2: v2-v1);
+    int x = x2 - x1;
+    int v = v2 - v1;
+    int n = x / v;
     return n;
 }
